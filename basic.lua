d53666f9e7686a28310cd3cc39e4a124e4a84bb6
xubury <xubury97@gmail.com>
2024-05-07 21:32:01 +0800

    Remove bug config

--- basic.lua
+++ basic.lua
@@ -1,11 +1,6 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 
-wezterm.on("gui-startup", function()
-    local tab, pane, window = mux.spawn_window({})
-    window:gui_window():maximize()
-end)
-
 return {
     default_prog = { "pwsh" },
 
