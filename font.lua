4266072366a709143bdbb656b4bee9bcf5dca6fc
xubury <xubury97@gmail.com>
2024-08-02 09:55:45 +0800

    Update

--- font.lua
+++ font.lua
@@ -6,7 +6,4 @@ return {
         weight = "Regular",
         harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
     }),
-    freetype_load_flags = "NO_HINTING|NO_AUTOHINT",
-    font_size = 12,
-    line_height = 1.0,
 }
