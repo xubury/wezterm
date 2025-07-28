bb13de13d9bb1f53003c97c9023bfa895f6ca854
xubury <xubury97@gmail.com>
2025-07-28 19:33:46 +0800

    remove time status

--- right-status.lua
+++ right-status.lua
@@ -97,9 +97,6 @@ end
 M.setup = function()
     wezterm.on("update-right-status", function(window, _pane)
         __cells__ = {}
-        _set_date()
-        _set_battery()
-
         window:set_right_status(wezterm.format(__cells__))
     end)
 end
