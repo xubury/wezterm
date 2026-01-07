96e0f8646ab185cfadbfe853089ca2033ecb96ba
buryxu <xubury97@gmail.com>
2026-01-07 15:49:30 +0800

    font

--- font.lua
+++ font.lua
@@ -2,9 +2,9 @@ local wezterm = require("wezterm")
 
 return {
     font = wezterm.font({
-        family = "JetBrainsMono NF",
+        family = "Sarasa Mono SC",
         harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
     }),
-    line_height = 1.11,
+    -- line_height = 1.11,
     use_resize_increments = true,
 }
