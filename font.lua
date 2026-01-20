2da534dcf7b810e56cae6810712e18e3bbc2e2e3
xubury <xubury97@gmail.com>
2026-01-20 15:19:48 +0800

    font

--- font.lua
+++ font.lua
@@ -5,6 +5,5 @@ return {
         family = "Sarasa Mono SC",
         harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
     }),
-    -- line_height = 1.11,
-    use_resize_increments = true,
+    font_size = 14,
 }
